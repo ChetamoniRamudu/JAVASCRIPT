164d8a785629803124fdbbd22c7332dd0779ea9b
ChetamoniRamudu <ramudu1307@gmail.com>
2022-01-24 11:34:05 +0530

    "finalsubmissionofobject"

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,13 @@
 function CreateObject(arr) {
     // Write your code here
+    var person={};
+    for(var i=1;i<arr.length;i+=2){
+       var x=arr[i-1];
+       var y=arr[i];
+       person[x]=y;
+
+    }
+    return person
 }
 
 module.exports = CreateObject;
